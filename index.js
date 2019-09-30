dda46f6bdef258e26b7e0bb0154e5ced63a4ce2a
luling <luling@smartspyeye.com.cn>
2019-09-30 09:22:08 +0800

    if node.js env, will start auto refresh config every 2 mins.

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 const _ = require("lodash");
 const axios = require("axios");
 const to = require('await-to-js').to;
-var loader = {};
-var configs = null;
+const jsEnv = require('browser-or-node');
+
+// global values
+var configs = null; //config data, return from config-center
+var version = null; //current config version, return from config-center
+
+// options
 var options = {
     remoteBaseUrl:"http://127.0.0.1", //required
-    configGroup:null, //optional
+    configGroup:null, //required
     API:"",
     body:{}, //optional
 
 
 };
 
+
+var loader = {};
 loader.getConfigValue =async function(configKey){
-
-    if(configs) return configs[configKey];
-
-    var axiosOps = {
-        method: "get",
-        url: options.API,
-        baseURL:options.remoteBaseUrl,
-        params: _.assign(options.body,{configGroup:options.configGroup}),
-        responseType: 'json',
-        responseEncoding: 'utf8',
-        timeout: 10000,
-        withCredentials: true
-    };
-
-    try{
-        let response;
-        response = await axios(axiosOps);
-        configs =  response.data;
-    }catch(error){
-        throw new Error(error.message);
-    }
-
+    var _that=this;
+    if(version) return configs[configKey];
+    await loadConfigs();
     return configs[configKey];
 };
 
@@ -47,5 +35,49 @@ loader.getOptions = function(){
 loader.getConfigs = function(){
     return configs;
 };
+
+//request config data from remote url
+async function loadConfigs(){
+    var axiosOps = {
+        method: "get",
+        url: options.API,
+        baseURL:options.remoteBaseUrl,
+        params: _.assign(options.body,{
+            configGroup:this.options.configGroup, //config group name
+        }),
+        responseType: 'json',
+        responseEncoding: 'utf8',
+        timeout: 10000,
+        withCredentials: true
+    };
+
+    try{
+        let response;
+        response = await axios(axiosOps);
+        let configs_data =  response.data;
+        if(configs_data.version==null){
+            configs = null;
+            version = null;
+            throw new Error("No available config version, you can set from Config-Center.");
+        }else{
+            configs = configs_data.configs;
+            version = configs_data.version;
+            console.log("Config reloaded: version "+version);
+        }
+    }catch(error){
+        throw new Error(error.message);
+    }
+}
+
+var schedule = require('node-schedule');
+// auto refresh, only for server side node
+if(jsEnv.isNode){
+    var _that = this;
+    //start schedule to refresh configs every minutes
+    var job = schedule.scheduleJob('1 * * * * *',async function(){
+        console.log('jsEnv is node, refresh configs every minutes');
+        await loadConfigs();
+    });
+}
 
 module.exports = loader;
